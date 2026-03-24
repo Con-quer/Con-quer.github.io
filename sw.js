c29a30f06bbc8589e14fd3cd6da7103092b882f2
Con-quer <2893590962@qq.com>
2026-03-24 16:33:18 +0800

    Site updated: 2026-03-24 16:33:18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1774339678849";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1774341197386";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
